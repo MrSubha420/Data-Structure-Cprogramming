0c218fcd7d15f3e50d6ba5066a35f7db83c15297
Subhadip Manna <codewithsubh2.0@gmail.com>
2025-11-17 13:59:02 +0530

    Add program to print rectangle star pattern

---
+++ basic c programming/5-Loop-lab4/pattern-programming/program1.c
@@ -0,0 +1,23 @@
+/* Print Rectangle Star Pattern
+   * * * * * *
+   * * * * * *
+   * * * * * *
+   * * * * * *
+   * * * * * *
+   * * * * * *
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for(i = 0; i < 6; i++) {
+        for(j = 0; j < 6; j++) {
+            printf(" *");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
